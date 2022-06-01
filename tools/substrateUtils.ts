343395e5c22730f1cd64e1ccbc77d2712b15c9eb
Gabriel <gabrieljaeger1@gmail.com>
2022-06-01 12:49:28 +0500

    remove the return

--- tools/substrateUtils.ts
+++ tools/substrateUtils.ts
@@ -265,7 +265,7 @@ export const sendAndFinalize = async (
       if (retry < MAX_RETRIES) {
         logger.info(`sendAndFinalize Retry #${retry} of ${MAX_RETRIES}`);
         await sleep(RETRY_DELAY_SECONDS * 1000);
-        return await sendAndFinalize(tx, account, resolvedOnFinalizedOnly, retry + 1);
+        await sendAndFinalize(tx, account, resolvedOnFinalizedOnly, retry + 1);
       } else {
         logger.error(`Error initiating tx signAndSend`, error);
         reject(error);
